7f3bc6a64fefa00f357a8e7fe397f01f60fd0ec3
JPascual007 <jonathanpascual007@gmail.com>
2023-05-10 02:24:09 -0700

    local storage and save button

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,7 @@ $('.time-block').each(function() {
    $(this).addClass('future');
   };
  });
-
+ 
 // TODO: Add a listener for click events on the save button. This code should
 // use the id in the containing time-block as a key to save the user input in
 // local storage. HINT: What does `this` reference in the click listener
@@ -30,6 +30,22 @@ $('.time-block').each(function() {
 // TODO: Add code to get any user input that was saved in localStorage and set
 // the values of the corresponding textarea elements. HINT: How can the id
 // attribute of each time-block be used to do this?
+
+$('.saveBtn').click(function() {
+  var value = $(this).siblings('.description').val();
+  var time = $(this).parent().attr('id');
+  localStorage.setItem(time, value);
+});
+
+ $('#hour-9 .description').val(localStorage.getItem('hour-9'));
+ $('#hour-10 .description').val(localStorage.getItem('hour-10'));
+ $('#hour-11 .description').val(localStorage.getItem('hour-11'));
+ $('#hour-12 .description').val(localStorage.getItem('hour-12'));
+ $('#hour-13 .description').val(localStorage.getItem('hour-13'));
+ $('#hour-14 .description').val(localStorage.getItem('hour-14'));
+ $('#hour-15 .description').val(localStorage.getItem('hour-15'));
+ $('#hour-16 .description').val(localStorage.getItem('hour-16'));
+ $('#hour-17 .description').val(localStorage.getItem('hour-17'));
 
 // TODO: Add code to display the current date in the header of the page.
 
